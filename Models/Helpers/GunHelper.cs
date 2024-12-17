210612d2757d975625a35d136a7c5f4c63911ee8
selva maramaei <119269320+selvamaramaei@users.noreply.github.com>
2024-12-17 23:51:35 +0300

    Add files via upload

---
+++ Models/Helpers/GunHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebProje.Helpers
+{
+    public static class GunHelper
+    {
+        public static readonly Dictionary<DayOfWeek, string> Gunler = new Dictionary<DayOfWeek, string>
+        {
+            { DayOfWeek.Monday, "Pazartesi" },
+            { DayOfWeek.Tuesday, "Salı" },
+            { DayOfWeek.Wednesday, "Çarşamba" },
+            { DayOfWeek.Thursday, "Perşembe" },
+            { DayOfWeek.Friday, "Cuma" },
+            { DayOfWeek.Saturday, "Cumartesi" },
+            { DayOfWeek.Sunday, "Pazar" }
+        };
+
+        public static string GetTurkceGun(DayOfWeek gun)
+        {
+            return Gunler.TryGetValue(gun, out var turkceGun) ? turkceGun : gun.ToString();
+        }
+    }
+}
