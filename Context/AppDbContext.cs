c533ab4a03870e1b2e071e1734f86b550f24e005
selva maramaei <119269320+selvamaramaei@users.noreply.github.com>
2024-12-17 23:49:01 +0300

    Add files via upload

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebProje.Models;
+
+namespace WebProje.Context
+{ 
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Islem> Islemler { get; set; }
+        public DbSet<Mesai> Mesailer { get; set; }
+        public DbSet<MesaiGunu> MesaiGunleri { get; set; }
+        public DbSet<Personel> Personeller { get; set; }
+        public DbSet<PersonelUzmanlik> Uzmanliklar { get; set; }
+        public DbSet<Randevu> Randevular { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext>options) : base(options)
+        {
+                
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Islem>()
+                .Property(i => i.Ucret)
+                .HasColumnType("decimal(18, 2)"); // Precision: 18, Scale: 2
+
+        }
+
+    }
+}
