f8e7dab8d8850295687b98cfbe224b9c090f2b9f
Elif Yerlikaya <122370715+elifyerlikaya@users.noreply.github.com>
2024-12-27 23:07:40 +0300

    AiOneri update

--- Models/AiOneri.cs
+++ Models/AiOneri.cs
@@ -12,18 +12,32 @@ namespace WebProje.Models
         [Required]
         [Display(Name = "Fotoğraf")]
         [DataType(DataType.Upload)]
-
         public IFormFile? Photo { get; set; }
 
 
         [Required]
         [Display(Name = "Saç Tipi")]
-        public string? HairStyle { get; set; }
+        public int? HairStyle { get; set; }
 
-        public List<int> HairStyles { get; set; } = new List<int>
+        [NotMapped]
+        public Dictionary<int, string> HairStyles { get; set; } = new Dictionary<int, string>
         {
-            101, 201, 301, 401, 402, 403, 502, 503, 603, 801, 901, 1001, 1101, 1201, 1301
-        };
+                { 101, "Bangs (default)" },
+                { 201, "Long hair" },
+                { 301, "Bangs with long hair" },
+                { 401, "Medium hair increase" },
+                { 402, "Light hair increase" },
+                { 403, "Heavy hair increase" },
+                { 502, "Light curling" },
+                { 503, "Heavy curling" },
+                { 603, "Short hair" },
+                { 801, "Blonde" },
+                { 901, "Straight hair" },
+                { 1001, "Oil-free hair" },
+                { 1101, "Hairline fill" },
+                { 1201, "Smooth hair" },
+                { 1301, "Fill hair gap" }
+         };
 
     }
 }
